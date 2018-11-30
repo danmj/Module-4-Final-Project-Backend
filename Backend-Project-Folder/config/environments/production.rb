da376279cb4ed1ec78a3c966e16691165834d6a5
Roger Vera <rovera@vassar.edu>
2018-11-30 15:02:59 -0500

    started backend from scratch

--- Backend-Project-Folder/config/environments/production.rb
+++ Backend-Project-Folder/config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "module-4-final-project-backend_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "Backend-Project-Folder_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
