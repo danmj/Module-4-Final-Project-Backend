dd6f23af843a9c18b15377a7b90e8ec20322bbf3
Roger Vera <rovera@vassar.edu>
2018-11-30 18:05:59 -0500

    generate and define Users controller

--- Backend-Project-Folder/app/controllers/api/v1/users_controller.rb
+++ Backend-Project-Folder/app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :find_user, only: [:update]
+
+  def index
+    @users = User.all
+    render json: @notes
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+    @user.update(user_params)
+    if @user.save
+      render json: @user, status: :accepted
+    else
+      render json: {errors: @user.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  def delete
+  end
+end
+
+private
+
+
+def user_params
+  params.permit(:first_name, :last_name, :email)
+end
+
+def find_user
+  @user = User.find(params[:id])
 end
