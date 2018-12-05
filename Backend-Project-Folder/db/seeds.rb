209dc02698ec01d45dde246b40eda47dece08ed4
danmj <daniel.jung@stern.nyu.edu>
2018-12-05 14:22:33 -0500

    Added seed data/images

--- Backend-Project-Folder/db/seeds.rb
+++ Backend-Project-Folder/db/seeds.rb
@@ -30,6 +30,36 @@ Photo.create({url: 'https://a0.muscache.com/im/pictures/78fc177e-7572-48d8-ae3b-
 
 Photo.create({url: 'https://a0.muscache.com/im/pictures/111e8df7-f41b-4a4a-b5af-c4211ccf9b15.jpg?aki_policy=x_large', imageable: l2})
 
+l3 = Listing.create({name: "Small apartment", owner: "Dominic Jones", city_id: 1, max_occupants: 6, pet_friendly: true, wifi: true, tv: true, pool: false, parking: false, kitchen: true, shared: false, price: 85.00, average_rating: 3.1, description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."})
+
+Photo.create({url: 'https://a0.muscache.com/im/pictures/2bf692ae-a17f-441b-b9b6-45f5ae9910bd.jpg?aki_policy=xx_large', imageable: l3})
+
+Photo.create({url: 'https://a0.muscache.com/im/pictures/220bf7bb-22f8-48d5-9c25-b047fb00a8ac.jpg?aki_policy=x_large', imageable: l3})
+
+Photo.create({url: 'https://a0.muscache.com/im/pictures/eb77c0a0-4901-477b-8392-4aca790bd197.jpg?aki_policy=x_large', imageable: l3})
+
+Photo.create({url: 'https://a0.muscache.com/im/pictures/5c4cd35f-6a7a-47eb-8a9c-7db0839608c1.jpg?aki_policy=x_large', imageable: l3})
+
+l4 = Listing.create({name: "Coolest room you'll see", owner: "Cole Anthony", city_id: 1, max_occupants: 2, pet_friendly: true, wifi: true, tv: true, pool: false, parking: false, kitchen: false, shared: true, price: 100.00, average_rating: 4.1, description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure."})
+
+Photo.create({url: 'https://a0.muscache.com/im/pictures/12426057/2292b61c_original.jpg?aki_policy=xx_large', imageable: l4})
+
+Photo.create({url: 'https://a0.muscache.com/im/pictures/12426125/9b046580_original.jpg?aki_policy=x_large', imageable: l4})
+
+Photo.create({url: 'https://a0.muscache.com/im/pictures/12426179/953baed3_original.jpg?aki_policy=x_large', imageable: l4})
+
+Photo.create({url: 'https://a0.muscache.com/im/pictures/5e123d5a-6aa6-4461-b2b5-6021f4bcd30d.jpg?aki_policy=x_large', imageable: l4})
+
+l5 = Listing.create({name: "The place to be", owner: "Janice Jacobs", city_id: 1, max_occupants: 8, pet_friendly: true, wifi: true, tv: false, pool: false, parking: false, kitchen: true, shared: true, price: 350.00, average_rating: 2.7, description: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish."})
+
+Photo.create({url: 'https://a0.muscache.com/im/pictures/63231627/9e6db8d1_original.jpg?aki_policy=xx_large', imageable: l5})
+
+Photo.create({url: 'https://a0.muscache.com/im/pictures/63231500/59af3ee1_original.jpg?aki_policy=x_large', imageable: l5})
+
+Photo.create({url: 'https://a0.muscache.com/im/pictures/63231593/8b413236_original.jpg?aki_policy=x_large', imageable: l5})
+
+Photo.create({url: 'https://a0.muscache.com/im/pictures/63231547/fc42427e_original.jpg?aki_policy=x_large', imageable: l5})
+
 
 # Good booking
 Booking.create({listing_id: 1, user_id: 1, start_date: "2018-12-04", end_date: "2018-12-09", guests: 2})
--- README.md
+++ README.md
@@ -15,4 +15,3 @@ $ rails db:migrate
 $ rails db:seed
 $ rails s
 ```
-
